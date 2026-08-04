d4fdd57801adb34df34a53a253059882e7f224cf
Mark Tasaka <mark.tasaka@gmail.com>
2026-08-04 13:00:35 -0700

    routing paths updated for character bios

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import Header from './inc/Header'
 import Footer from './inc/Footer'
 import Home from './pages/Home'
 import CharacterBios from './pages/CharacterBios'
+// import SnakeEyes from './pages/characters/SnakeEyes'
+import Scarlett from './pages/characters/Scarlett'
+// import SoftMaster from './pages/characters/SoftMaster'
+// import BlindMaster from './pages/characters/BlindMaster'
 import ComicIssues from './pages/ComicIssues'
 import Resources from './pages/Resources'
 import Tribute from './pages/Tribute'
@@ -18,6 +22,10 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/character-bios" element={<CharacterBios />} />
+          {/* <Route path="/character-bios/SnakeEyes" element={<SnakeEyes />} /> */}
+          <Route path="/character-bios/Scarlett" element={<Scarlett />} />
+          {/* <Route path="/character-bios/SoftMaster" element={<SoftMaster />} />
+          <Route path="/character-bios/BlindMaster" element={<BlindMaster />} /> */}
           <Route path="/comic-issues" element={<ComicIssues />} />
           <Route path="/resources" element={<Resources />} />
           <Route path="/tribute" element={<Tribute />} />
