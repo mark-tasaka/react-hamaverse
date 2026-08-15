865c17e0884631b943a2ef35be3e89c2c9350b19
Mark Tasaka <mark.tasaka@gmail.com>
2026-08-14 19:00:53 -0700

    Onihashi links added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -27,7 +27,7 @@ function Home() {
           <h2 className="update-log-title">Update Log</h2>
           <p className="update-log-date">August 12, 2026</p>
           <p className="update-log-entry">
-            The character bios for <Link to="/character-bios/Hawk">Hawk</Link>, <Link to="/character-bios/Wade">Wade Collins</Link> and <Link to="/character-bios/Marina">Marina</Link> have been added. More sections added to <Link to="/character-bios/StormShadow">Storm Shadows</Link>'s bio. As with Storm Shadow's bio, I will be adding more sections to Hawk and Wade Collins' bios.
+            The character bio for <Link to="/character-bios/Onihashi">Professor Onihashi</Link> and expanded bios for <Link to="/character-bios/Wade">Wade Collins</Link> by adding more sections.  Added filter buttons to the character bios page to allow users to filter characters by faction and status.  Added a search bar to the character bios page to allow users to search for characters by name.
           </p>
 
           <div className="btn-row">
